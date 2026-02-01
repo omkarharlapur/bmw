8ac21d1fd9b7707ca6c1086d3332aaf0138ffbd4
omkarharlapur <omzharlapur@gmail.com>
2026-02-01 09:05:57 +0530

    video cards added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,55 @@
+import { useRef } from "react"
+import { useState } from "react"
 import "./App.css"
+
 export default function Home(){
+
+    const[cursor,setCursor]=useState({show:false,x:0,y:0,text:""})
+
+
+
+    const video1=useRef();
+    const video2=useRef();
+    const video3=useRef();
+
+    const playVideo=(ref,index)=>{
+        
+        ref.current.play()
+    }
+    
+    const pauseVideo=(ref)=>{
+        ref.current.pause()
+    }
+
     return(
-        <div>
-            <div className="backgroundImage">
-                <img src="/bmwMain.jpg" alt="" />
+        <div className="hero-container">
+            <div className="intro-video">
+                <video src="/13103349_1920_1080_60fps.mp4" autoPlay muted></video>
             </div>
-               <div className="backgroundImage">
-                <img src="/pexels-audrius-strikaitis-31613974-10903097.jpg" alt="" />
+            <div className="divider">
+                <h1>A GERMAN MACHINE</h1>
             </div>
-            
+        {cursor.show && (
+            <div className="floating-cursor" style={{left:cursor.x+15,top:cursor.y+15}}>
+                {cursor.text}
+            </div>
+        )}
+            <div className="exp-card">
+                <video src="/cust.mp4" ref={video1} width="100%" height="auto" muted 
+                onMouseEnter={()=>playVideo(video1,1)}
+                onMouseLeave={()=>pauseVideo(video1)}
+                ></video>
+               <video src="/meter.mp4" width="100%" ref={video2} height="auto"  muted
+               onMouseEnter={()=>playVideo(video2,2)}
+                onMouseLeave={()=>pauseVideo(video2)} ></video>
+               <video src="/bentley.mp4" width="100%" ref={video3} height="auto"  muted
+               onMouseEnter={()=>playVideo(video3,3)}
+                onMouseLeave={()=>pauseVideo(video3)} ></video>
+            </div>
+
         </div>
+          
+    
 
     )
 }
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -5,16 +5,17 @@ import Profile from "./Profile";
 import About from "./About"
 export default function Navbar(){
     return (
-        <div className="nav-container">
+        <nav>
             <div className="logo">
-                <img src="/bmw_white_logo.svg" alt="" />
+                <img src="bmw_white_logo.svg" alt="" />
             </div>
-            <ul className="nav-links">
-                <li><NavLink to={"/"}>Home</NavLink></li>
-                <li><NavLink to={"/profile"}>Profile</NavLink></li>
-                <li><NavLink to={"/about"}>About</NavLink></li>
-            </ul>
-           
-        </div>
+            <div className="nav-container">
+                <ul className="nav-links">
+                    <li><NavLink to={"/"}>HOME</NavLink></li>
+                    <li><NavLink to={"/about"}>ABOUT</NavLink></li>
+                    <li><NavLink to={"/profile"}>PROFILE</NavLink></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
